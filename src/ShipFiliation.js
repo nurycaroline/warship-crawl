956623311f555b8f3a2322f06be7af8067378a3c
Nurielly Caroline Brizola <nurycaroline@gmail.com>
2022-09-22 18:10:26 -0300

    Listando categorias

---
+++ src/ShipFiliation.js
@@ -0,0 +1,33 @@
+const Puppeteer = require('puppeteer');
+const fs = require('fs');
+
+async function run() {
+	const browser = await Puppeteer.launch()
+
+	const page = await browser.newPage()
+	await page.goto('https://starwars.fandom.com/pt/wiki/Categoria:Naves_por_afiliação', { timeout: 0 })
+
+	const links = await page.evaluate((props) => {
+		const tagLinks = document.getElementsByClassName('category-page__member-link')
+
+		const nameCategories = []
+
+		for (let index = 0; index < tagLinks.length; index++) {
+			const element = tagLinks[index];
+			nameCategories.push(element.innerText)
+		}
+
+		return nameCategories
+	})
+
+	console.log({
+		links
+	})
+
+	// let data = JSON.stringify(links);
+ 	// fs.writeFileSync('./links.json', data);s
+
+	await browser.close()
+}
+
+run()
